4b85e137b8b318a2df93f30c336c6b3cad13d428
AjBreidenbach <breidenbach.aj@gmail.com>
2018-05-06 14:45:39 -0500

    just realized something was redundant

--- reader.js
+++ reader.js
@@ -11,18 +11,18 @@ function createReader(callback) {
 				data = evt.target.result,
 				lines = data.split(/\r*\n/).filter( l => (/^(-?\d+(\.\d+\s*,\s*)?){2}\d+$/).test(l) ),
 				seperator = /\s*,\s*/,
-				 results = []
+				results = []
 			
 
-			for (var i = 0; i < lines.length; i++) {
-				let columns = lines[i].split(seperator)
-				if (columns.length > 1)  results.push({
+			for (line in lines) {
+				let columns = line.split(seperator)
+				results.push({
 					latitude: columns[0],
 					longitude: columns[1],
 					lux: columns[2],
 				})
 			}
-			if (lines.length == 0) alert('Could not read any records :(')
+			if (results.length == 0) alert('Could not read any records :(')
 			else callback(results)
 		}
 
